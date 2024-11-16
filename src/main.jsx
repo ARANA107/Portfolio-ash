7bb80a7bb3361dabe837c1d92918144497c08aee
aishwarya rana <ashrana1993@gmail.com>
2024-11-15 20:58:32 -0500

    o

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-// main.jsx
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx"; // Import the main app component
-import "./index.css"; // Optional: Import global styles
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-// Create a root and render the App component
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
